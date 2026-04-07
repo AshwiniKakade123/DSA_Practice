d7c5b1004a73b447f0976edaa5198ef0b5a4142d
AshwiniKakade123 <kakadeashwini81@gmail.com>
2026-04-07 16:05:23 +0530

    find area

---
+++ basic_code/Area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Area {
+
+    public static void main(String args[])
+    {
+           Scanner sc=new Scanner(System.in);
+           int radius=sc.nextInt();
+           double pi=3.14;
+           double area= pi*radius*radius;
+           System.out.println(area);
+    }
+}
