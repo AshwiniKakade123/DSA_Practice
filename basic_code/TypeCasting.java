fe4f4d081c48d11895d4a067e4249b3bd3bd105c
AshwiniKakade123 <kakadeashwini81@gmail.com>
2026-04-07 22:57:07 +0530

    type casting

---
+++ basic_code/TypeCasting.java
@@ -0,0 +1,15 @@
+public class TypeCasting {
+
+     public static void main(String args[])
+    {
+        int a=12;
+        float b=(float)a;
+        
+        double c=25.22;
+        int d=(int)c;
+
+        System.out.println(d);
+        System.out.println(b);
+    }
+    
+}
